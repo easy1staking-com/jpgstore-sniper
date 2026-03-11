fd20aac7ff8a6c52832287b137bffc8cd252fc45
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2026-03-11 09:11:22 +0000

    chore: added test

---
+++ src/jpgstore-sniper-offchain/src/test/java/com/easy1staking/jpgstore/deployment/LargeDatumTest.java
@@ -0,0 +1,56 @@
+package com.easy1staking.jpgstore.deployment;
+
+import com.bloxbean.cardano.aiken.AikenTransactionEvaluator;
+import com.bloxbean.cardano.client.account.Account;
+import com.bloxbean.cardano.client.api.model.Amount;
+import com.bloxbean.cardano.client.api.util.ValueUtil;
+import com.bloxbean.cardano.client.common.model.Network;
+import com.bloxbean.cardano.client.common.model.Networks;
+import com.bloxbean.cardano.client.function.helper.SignerProviders;
+import com.bloxbean.cardano.client.plutus.spec.BytesPlutusData;
+import com.bloxbean.cardano.client.plutus.spec.ListPlutusData;
+import com.bloxbean.cardano.client.quicktx.Tx;
+import com.bloxbean.cardano.client.transaction.spec.Asset;
+import com.bloxbean.cardano.client.util.HexUtil;
+import com.easy1staking.jpgstore.jpgstorejavaclient.AbstractTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Slf4j
+public class LargeDatumTest extends AbstractTest {
+
+    @Test
+    public void mintSnipeTest() throws Exception {
+
+        var account = Account.createFromMnemonic(Networks.mainnet(), MNEMONIC);
+        log.info("Account {}", account.baseAddress());
+
+        var hundredNfts = IntStream.range(0, 30).mapToObj(i -> "a706fc87764cde4ac018c38bf61630c1065932db49e6f495be3b29f8436f636f4c6f636f426c756550616c6d53314e4654313737")
+                .map(unit -> BytesPlutusData.of(HexUtil.decodeHexString(unit)))
+                .toList();
+
+        var nfts  = ListPlutusData.of(hundredNfts.toArray(new BytesPlutusData[]{}));
+
+        var tx = new Tx()
+                .from(account.baseAddress())
+                .payToContract(account.baseAddress(), List.of(Amount.ada(1)), nfts)
+                .withChangeAddress(account.baseAddress());
+
+        var transaction = QUICK_TX_BUILDER.compose(tx)
+                .feePayer(account.baseAddress())
+                .withSigner(SignerProviders.signerFrom(account))
+                .mergeOutputs(false)
+                .buildAndSign();
+
+        log.info("transaction: {}", transaction.serializeToHex());
+
+        log.info("transaction: {}", OBJECT_MAPPER.writeValueAsString(transaction));
+
+    }
+
+}
